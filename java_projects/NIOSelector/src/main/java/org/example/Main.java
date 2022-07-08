89c95ce72b33e1c2debe677df371ff7acc00ff33
dohyunkim <kdh951102@gmail.com>
2022-07-08 18:14:19 +0900

    Add NIOSelector

--- java_projects/NIOSelector/src/main/java/org/example/Main.java
+++ java_projects/NIOSelector/src/main/java/org/example/Main.java
@@ -1,10 +1,61 @@
 package org.example;
 
-import java.nio.channels.Selector;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Set;
 
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final String EXIT = "EXIT";
+
+    public static void main(String[] args) throws IOException {
+        try {
+            Selector selector = Selector.open();
+            ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.bind(new InetSocketAddress("localhost", 3000));
+            serverSocket.configureBlocking(false);
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+            ByteBuffer buffer = ByteBuffer.allocate(256);
+
+            while(true){
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+                while(iter.hasNext()){
+                    SelectionKey key = iter.next();
+
+                    if(key.isAcceptable()){
+                        register(selector, serverSocket);
+                    }
+                    if(key.isReadable()){
+                        answerWithEcho(buffer, key);
+                    }
+                    iter.remove();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private static void answerWithEcho(ByteBuffer buffer, SelectionKey key) throws IOException {
+        SocketChannel client = (SocketChannel) key.channel();
+        client.read(buffer);
+        if(new String(buffer.array()).trim().equals(EXIT)){
+            client.close();
+            System.out.println("Not accepting client message anymore");
+        }
+        buffer.flip();
+        client.write(buffer);
+        buffer.clear();
+    }
+    private static void register(Selector selector, ServerSocketChannel serverSocket) throws IOException {
+        SocketChannel client = serverSocket.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ);
+        System.out.println("New Client connected..");
     }
 }
